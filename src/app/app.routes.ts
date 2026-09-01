3bba4d96b80160ea928d38e662bf3f21d788acd6
Nagaraj841 <nagaraj.v@c2c-as.com>
2026-09-01 14:51:14 +0530

    thorught appication icons and has changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1277,6 +1277,11 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/Superadmin/superadmin-manage-users/superadmin-manage-users').then(m => m.SuperadminManageUsers)
   },
+  {
+    path: 'superadmin/superadmin-manage-users',
+    loadComponent: () =>
+      import('./pages/Superadmin/superadmin-manage-users/superadmin-manage-users').then(m => m.SuperadminManageUsers)
+  },
 
 {
           path: 'superadmin/add-manage-user',
